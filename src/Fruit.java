bf8bdd2702906f5f4e4b71408d13e4905aa3390b
Olesya <popovska.olesia@gmail.com>
2022-12-14 17:50:27 +0200

    fruit

---
+++ src/Fruit.java
@@ -0,0 +1,11 @@
+public class Fruit {
+    String name;
+    String color;
+
+    void Eat() {
+        System.out.println(name + " was delicious!");
+    }
+    void Look() {
+        System.out.println(name + " is " + color +" color ");
+    }
+}
---
+++ src/HomeWork8.java
@@ -0,0 +1,35 @@
+public class HomeWork8 {
+    public static void main(String[] args) {
+        Fruit F1 = new Fruit();
+        F1.name = "Apple";
+        F1.color = "red";
+        F1.Look();
+        F1.Eat();
+
+        Fruit F2 = new Fruit();
+        F2.name = "Tamarillo";
+        F2.color = "dark red";
+        F2.Look();
+        F2.Eat();
+
+
+        Fruit F3 = new Fruit();
+        F3.name = "Mango";
+        F3.color = "yellow";
+        F3.Look();
+        F3.Eat();
+
+        Fruit F4 = new Fruit();
+        F4.name = "Kivano";
+        F4.color = "bright yellow";
+        F4.Look();
+        F4.Eat();
+
+
+        Fruit F5 = new Fruit();
+        F5.name = "Pineapple";
+        F5.color = "green yellow";
+        F5.Look();
+        F5.Eat();
+    }
+}
